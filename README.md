c2b7f1c5c11589c67d2bfdfa6f10da016609c362
umutyoldaa <88593075+umutyoldaa@users.noreply.github.com>
2026-04-09 09:52:15 +0200

    2. Lesson, wir install new package and write more CRUD functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fullstack-mern-tutorial
-My implementation of the MERN stack notes app tutorial from freeCodeCamp. Exploring full-stack development, CRUD operations, and state management in React.
+My implementation of the MERN stack notes app tutorial from freeCodeCamp.
+https://www.youtube.com/watch?v=F9gB5b4jgOI&t=1564s
+ Exploring full-stack development, CRUD operations, and state management in React.
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
+
 const app = express();
+
 //req = request, res = response
 app.get("/api/notes",(req,res) =>{
-    res.status(200).send("You got 5 notes");
+    res.status(200).send("You got 30 notes");
 });
-app.
+
+app.post(/api/notes,(req,res) =>{
+    res.status(201).json(({
+        message: "Note created successfully"
+    }));
+});
+app.put("/api/notes/:id", (req,res) =>{
+    res.status(200).json({
+        message: "Note updated successfully"
+    });
+});
+
+app.delete("/api/notes/:id", (req,res) =>{
+    res.status(200).json({
+        message: "Note deleted successfully"
+    });
+});
+
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
 });
