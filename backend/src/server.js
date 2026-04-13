4e0d6dfbf817b2ef13f3844bff41380b1a929c9a
umutyoldaa <88593075+umutyoldaa@users.noreply.github.com>
2026-04-13 23:02:30 +0200

    Middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,33 @@ import express from 'express';
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001 ;
-connectDB();
 
-//middleware
+//middleware, It allows us to  get req.body as JSON object in our Routers. 
+// It parses incoming JSON request bodies and makes the data available in req.body. 
+// Without this middleware, req.body would be undefined for JSON requests.
 app.use(express.json()); //to parse JSON request bodies
+app.use(rateLimiter); // Apply the rate limiter middleware to all routes
+
+app.use((req, res, next) => {
+  console.log(`Req method is ${req.method} and req url is ${req.url} `);
+  next();
+}); 
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
   console.log('Server is running on port:',PORT);
 });
+});
+
+
 
 
 
