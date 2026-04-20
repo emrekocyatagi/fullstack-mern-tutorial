0649dbc9dd26c11627ac7dc7e553b4570dafe5de
umutyoldaa <88593075+umutyoldaa@users.noreply.github.com>
2026-04-20 16:29:23 +0200

    feat: enhance server configuration for production and development environments, add axios base URL setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -11,8 +12,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001 ;
-
-app.use(cors({ origin: "http://localhost:5173" }));
+const __dirname = path.resolve();
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 //middleware, It allows us to  get req.body as JSON object in our Routers. 
 // It parses incoming JSON request bodies and makes the data available in req.body. 
 // Without this middleware, req.body would be undefined for JSON requests.
@@ -28,6 +31,13 @@ app.use((req, res, next) => {
 }); 
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// In development, we want to use the local backend server, and in production, we want to use the same origin (which will be the deployed backend server).
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
